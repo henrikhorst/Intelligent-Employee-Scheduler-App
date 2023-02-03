5a076cc17eb373a391557fe2ca98023e1620adfe
henrikhorst <henrik.ho@gmx.de>
2023-02-03 08:51:15 +0100

    All files

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ root.maxsize(500, 500)
 root.geometry("300x300+50+50")  # width x height + x + y
 
 # Create Label in our window
-text = tk.Label(root, text="Nothing will work unless you do.")
+text = ttk.Label(root, text="Nothing will work unless you do.")
 text.pack()
-text2 = tk.Label(root, text="- Maya Angelou")
+text2 = ttk.Label(root, text="- Maya Angelou")
 text2.pack()
+
+# Create Frame widget
+left_frame = tk.Frame(root, width=200, height=400)
+left_frame.grid(row=0, column=0, padx=10, pady=5)
 
 root.mainloop()
